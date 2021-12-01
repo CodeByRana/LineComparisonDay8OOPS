72715fe3aae44d993a2497709bbcc3eaecda89a1
deepakphpweb <dipakrana2760@gmail.com>
2021-12-02 01:01:25 +0530

    Added By Rana : Create a LineComparison Program Using use case 3

--- src/com/bridgelabz/linecom/LineComparison.java
+++ src/com/bridgelabz/linecom/LineComparison.java
@@ -11,41 +11,45 @@ public class LineComparison {
             this.x2 = x2;
             this.y2 = y2;
         }
-        public void checkEquality(Cordinates cordinates) {
+
+        public void compareLength(Cordinates cordinates) {
             Double lengthOfLine1 = Math.sqrt(Math.pow((this.x2 - this.x1),2) + Math.pow((this.y2 - this.y1),2)),
                     lengthOfLine2 = Math.sqrt(Math.pow((cordinates.x2 - cordinates.x1),2) + Math.pow((cordinates.y2 - cordinates.y1),2));
-            if(lengthOfLine1.equals(lengthOfLine2)) {
-                System.out.println("Both lines have equal length!");
+
+            if(lengthOfLine1.compareTo(lengthOfLine2) == 0) {
+                System.out.println("Both lines have equal length");
+            } else if(lengthOfLine1.compareTo(lengthOfLine2) > 0) {
+                System.out.println("1st line is greater than 2nd line");
             } else {
-                System.out.println("Both lines have un2equal length!");
+                System.out.println("2nd line is greater than 1st line");
             }
         }
     public static void main(String[] args) {
         int x1, y1, x2, y2;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter co-ordinates of 1st point of 1st line : ");
+        System.out.println("Enter co-ordinates of 1st point of 1st line ");
         System.out.println("x : ");
         x1 = sc.nextInt();
         System.out.println("y : ");
         y1 = sc.nextInt();
-        System.out.println("Enter co-ordinates of 2nd point of 1st line : ");
+        System.out.println("Enter co-ordinates of 2nd point of 1st line ");
         System.out.println("x : ");
         x2 = sc.nextInt();
         System.out.println("y : ");
         y2 = sc.nextInt();
         Cordinates cordinatesOfLine1 = new Cordinates(x1, y1, x2, y2);
-        System.out.println("Enter co-ordinates of 1st point of 2nd line : ");
+        System.out.println("Enter co-ordinates of 1st point of 2nd line ");
         System.out.println("x : ");
         x1 = sc.nextInt();
-        System.out.println("y :");
+        System.out.println("y : ");
         y1 = sc.nextInt();
-        System.out.println("Enter co-ordinates of 2nd point of 2nd line : ");
+        System.out.println("Enter co-ordinates of 2nd point of 2nd line ");
         System.out.println("x : ");
         x2 = sc.nextInt();
         System.out.println("y : ");
         y2 = sc.nextInt();
         Cordinates cordinatesOfLine2 = new Cordinates(x1, y1, x2, y2);
-        cordinatesOfLine1.checkEquality(cordinatesOfLine2);
+        cordinatesOfLine1.compareLength(cordinatesOfLine2);
     }
     }
 }
